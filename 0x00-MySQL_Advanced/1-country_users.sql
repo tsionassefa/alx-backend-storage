3b327ae253bc1e732e3ccffcd9fe6eca692852b6
tsion <tsionassefa101@gmail.com>
2023-08-10 12:45:56 +0300

    tsi

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,5 +4,5 @@ CREATE TABLE users(
    id int NOT NULL PRIMARY KEY AUTO_INCREAMENT,
    email VARCHAR(255) NOT NULL UNIQUE,
    name VARCHAR(255),
-   country ENUM('US', 'CO','TN') NOT NULL DEFAULT 'US'
+   country char(2) CHECK(country('US', 'CO','TN')) NOT NULL DEFAULT 'US'
 )
