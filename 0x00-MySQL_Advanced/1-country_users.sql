4dc2bb115704ae84fb7ce1469c33f3bee8e59a8a
tsion <tsionassefa101@gmail.com>
2023-08-10 12:47:11 +0300

    done

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
--- Create table with ENUM
+-- Creates a table with unique users.
 DROP TABLE IF EXISTS users;
-CREATE TABLE users(
-   id int NOT NULL PRIMARY KEY AUTO_INCREAMENT,
-   email VARCHAR(255) NOT NULL UNIQUE,
-   name VARCHAR(255),
-   country char(2) CHECK(country('US', 'CO','TN')) NOT NULL DEFAULT 'US'
-)
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+);
